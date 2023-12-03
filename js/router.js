cccae6a3f542a63b50a9af98eed83a15b0e680b2
Onias <onias.magdalena@gmail.com>
2023-12-03 21:32:27 +0000

    Initial implementations

---
+++ js/router.js
@@ -0,0 +1,25 @@
+const route = (event) => {
+     event = event || window.event;
+     event.preventDefault();
+     window.history.pushState(null, null, event.target.href);
+     handleLocation();
+};
+
+const routes = {
+404: "/pages/404.html",
+"/": "/pages/index.html",
+"/Biography": "/pages/biography.html",
+"/Education": "/pages/education.html"
+};
+
+const handleLocation = async () => {
+     const path = window.location.pathname;
+     const route = routes[path] || routes[404];
+     const html = await fetch(route).then((data) => data.text());
+     document.getElementById("main-content").innerHTML = html;
+   };
+
+window.onpopstate = handleLocation;
+window.route = route;
+
+handleLocation();
--- js/script.js
+++
@@ -1,4 +0,0 @@
-// You can add JavaScript functionality here if needed
-// For a simple "About Me" page, you might not need JavaScript
-
-// Example: console.log("Hello from script.js");
